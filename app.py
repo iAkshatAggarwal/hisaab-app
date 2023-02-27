c346fd4e7390f7e705a6f4e3295d6785e89f705c
Akshat Aggarwal <71456993+iAkshatAggarwal@users.noreply.github.com>
2023-02-27 16:56:09 +0000

    added nav and footer

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 # from flask_sqlalchemy import SQLAlchemy
 import datetime
-import pandas as pd
-from sklearn.linear_model import LinearRegression
+# import pandas as pd
+# from sklearn.linear_model import LinearRegression
 from database import load_inventory
 
 app = Flask(__name__)
@@ -21,61 +21,61 @@ app = Flask(__name__)
 #     date = db.Column(db.Date, nullable=False)
 #     quantity = db.Column(db.Integer, nullable=False)
 
-def predict_growth():
-    sales_data = pd.read_sql_table('sale', con=db.engine)
-    sales_data['month'] = pd.to_datetime(sales_data['date']).dt.to_period('M')
-    monthly_sales = sales_data.groupby('month')['quantity'].sum().reset_index()
-    X = pd.to_numeric((monthly_sales['month'] - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))
-    X = X.values.reshape(-1, 1)
-    y = monthly_sales['quantity']
-    model = LinearRegression()
-    model.fit(X, y)
-    next_month = pd.Period(datetime.date.today(), freq='M') + 1
-    next_month_sales = model.predict([[pd.to_numeric((next_month - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))]])
-    return next_month_sales[0]
+# def predict_growth():
+#     sales_data = pd.read_sql_table('sale', con=db.engine)
+#     sales_data['month'] = pd.to_datetime(sales_data['date']).dt.to_period('M')
+#     monthly_sales = sales_data.groupby('month')['quantity'].sum().reset_index()
+#     X = pd.to_numeric((monthly_sales['month'] - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))
+#     X = X.values.reshape(-1, 1)
+#     y = monthly_sales['quantity']
+#     model = LinearRegression()
+#     model.fit(X, y)
+#     next_month = pd.Period(datetime.date.today(), freq='M') + 1
+#     next_month_sales = model.predict([[pd.to_numeric((next_month - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))]])
+#     return next_month_sales[0]
 
 @app.route('/')
 def index():
     products = load_inventory()
-    return render_template('index.html', products=products)
+    return render_template('home.html', products=products)
 
-@app.route('/product/add', methods=['GET', 'POST'])
-def add_product():
-    if request.method == 'POST':
-        name = request.form['name']
-        price = float(request.form['price'])
-        product = Product(name=name, price=price)
-        db.session.add(product)
-        db.session.commit()
-        return redirect(url_for('index'))
-    else:
-        return render_template('add_product.html')
+# @app.route('/product/add', methods=['GET', 'POST'])
+# def add_product():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         price = float(request.form['price'])
+#         product = Product(name=name, price=price)
+#         db.session.add(product)
+#         db.session.commit()
+#         return redirect(url_for('index'))
+#     else:
+#         return render_template('add_product.html')
 
-@app.route('/sale/add', methods=['GET', 'POST'])
-def add_sale():
-    if request.method == 'POST':
-        product_id = int(request.form['product'])
-        product = Product.query.get(product_id)
-        date = datetime.datetime.strptime(request.form['date'], '%Y-%m-%d').date()
-        quantity = int(request.form['quantity'])
-        sale = Sale(product=product, date=date, quantity=quantity)
-        db.session.add(sale)
-        db.session.commit()
-        return redirect(url_for('index'))
-    else:
-        products = Product.query.all()
-        return render_template('add_sale.html', products=products)
+# @app.route('/sale/add', methods=['GET', 'POST'])
+# def add_sale():
+#     if request.method == 'POST':
+#         product_id = int(request.form['product'])
+#         product = Product.query.get(product_id)
+#         date = datetime.datetime.strptime(request.form['date'], '%Y-%m-%d').date()
+#         quantity = int(request.form['quantity'])
+#         sale = Sale(product=product, date=date, quantity=quantity)
+#         db.session.add(sale)
+#         db.session.commit()
+#         return redirect(url_for('index'))
+#     else:
+#         products = Product.query.all()
+#         return render_template('add_sale.html', products=products)
 
-@app.route('/revenue')
-def revenue():
-    sales_data = pd.read_sql_table('sale', con=db.engine)
-    revenue = (sales_data['quantity'] * sales_data['product'].apply(lambda x: x.price)).sum()
-    return render_template('revenue.html', revenue=revenue)
+# @app.route('/revenue')
+# def revenue():
+#     sales_data = pd.read_sql_table('sale', con=db.engine)
+#     revenue = (sales_data['quantity'] * sales_data['product'].apply(lambda x: x.price)).sum()
+#     return render_template('revenue.html', revenue=revenue)
 
-@app.route('/growth')
-def growth():
-    next_month_sales = predict_growth()
-    return render_template('growth.html', next_month_sales=next_month_sales)
+# @app.route('/growth')
+# def growth():
+#     next_month_sales = predict_growth()
+#     return render_template('growth.html', next_month_sales=next_month_sales)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
