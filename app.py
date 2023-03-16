d8e7c96cceac5c0555222697b8eb60d85f3b0201
Akshat Aggarwal <71456993+iAkshatAggarwal@users.noreply.github.com>
2023-03-16 16:03:35 +0000

    added register page

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def index():
 def login():
     return render_template('login.html') 
 
+@app.route('/register')
+def register():
+    return render_template('register.html') 
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
