29fbd16e326a8db521edfad0a20ae4f6904f035f
Akshat Aggarwal <71456993+iAkshatAggarwal@users.noreply.github.com>
2023-03-31 05:57:28 +0000

    added authentication using sessions

--- app.py
+++ app.py
@@ -2,24 +2,11 @@ import os
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 import datetime
 from utils import check_user, make_chart
-from database import load_users, load_inventory, load_sales, load_wholesalers, delete_products
-# from sklearn.linear_model import LinearRegression
+from database import load_users, load_inventory, load_sales, load_wholesalers
 
 app = Flask(__name__)
 app.secret_key = os.environ['SECRET_KEY']
 
-# def predict_growth():
-#     sales_data = pd.read_sql_table('sale', con=db.engine)
-#     sales_data['month'] = pd.to_datetime(sales_data['date']).dt.to_period('M')
-#     monthly_sales = sales_data.groupby('month')['quantity'].sum().reset_index()
-#     X = pd.to_numeric((monthly_sales['month'] - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))
-#     X = X.values.reshape(-1, 1)
-#     y = monthly_sales['quantity']
-#     model = LinearRegression()
-#     model.fit(X, y)
-#     next_month = pd.Period(datetime.date.today(), freq='M') + 1
-#     next_month_sales = model.predict([[pd.to_numeric((next_month - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))]])
-#     return next_month_sales[0]
 
 @app.route('/')
 def index():
@@ -31,48 +18,71 @@ def login():
         return render_template("login.html")
       
     elif request.method == "POST": 
-        session['login_error'] = False
         users = load_users()
-        username = request.form["username"]
-        password = request.form["password"]
-      
-        if check_user(users, username, password):
-            return render_template("dashboard.html")
-        login_error = session.pop('login_error', False)
-        return render_template('login.html', login_error=login_error)
+        if check_user(users, 
+                      request.form["username"],
+                      request.form["password"]):
+            session['authenticated'] = True
+            return redirect(url_for('dashboard'))
+                        
+        else: 
+            return render_template('login.html', login_error = True)
 
+@app.route('/logout')
+def logout():
+    # clear session variables
+    session.clear()
+    # redirect to login page
+    return redirect(url_for('login'))
+          
 #Dashboard
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    # check if user is authenticated
+    if session.get('authenticated'):
+      # user is authenticated, render dashboard page
+        return render_template('dashboard.html')
+    else:
+        # user is not authenticated, redirect to login page
+        return redirect(url_for('login'))
 
 #Products
 @app.route('/products')
 def show_products():
-  products = load_inventory()
-  #For chart
-  data = make_chart(products, 'pname', 'pqty')
-  return render_template('products.html',
-                         products=products,
-                         data=data)
-
+  if session.get('authenticated'):
+      products = load_inventory()
+      #For chart
+      data = make_chart(products, 'pname', 'pqty')
+      return render_template('products.html',
+                             products=products,
+                             data=data)
+  else:
+      return redirect(url_for('login'))
+    
 # @app.route("/products/<int:pid>/delete")
-# delete_products(id)
+# def delete_products(id)
 
 #Ledgers
 @app.route('/ledgers')
 def show_ledgers():
-  ledgers = load_wholesalers()
-  #For chart
-  data = make_chart(ledgers, 'wname', 'credit')
-  return render_template('ledger.html',
-                         ledgers=ledgers,
-                         data=data)
-
+  if session.get('authenticated'):
+      ledgers = load_wholesalers()
+      #For chart
+      data = make_chart(ledgers, 'wname', 'credit')
+      return render_template('ledger.html',
+                             ledgers=ledgers,
+                             data=data)
+  else:
+      return redirect(url_for('login'))
+    
 @app.route('/orders')
 def show_sales():
-  sales = load_sales()
-  return render_template('sales.html', sales=sales)
+  if session.get('authenticated'):
+      sales = load_sales()
+      return render_template('sales.html', sales=sales)
+
+  else:
+      return redirect(url_for('login'))
 
 # @app.route('/product/add', methods=['GET', 'POST'])
 # def add_product():
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+# from sklearn.linear_model import LinearRegression
+
 def check_user(users, username, password):
   for user in users:
     # users is a list of dictionary
@@ -13,3 +15,16 @@ def make_chart(table, x, y):
     values.append(row[y])
   data = {'labels':labels, 'values':values}
   return data
+
+# def predict_growth():
+#     sales_data = pd.read_sql_table('sale', con=db.engine)
+#     sales_data['month'] = pd.to_datetime(sales_data['date']).dt.to_period('M')
+#     monthly_sales = sales_data.groupby('month')['quantity'].sum().reset_index()
+#     X = pd.to_numeric((monthly_sales['month'] - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))
+#     X = X.values.reshape(-1, 1)
+#     y = monthly_sales['quantity']
+#     model = LinearRegression()
+#     model.fit(X, y)
+#     next_month = pd.Period(datetime.date.today(), freq='M') + 1
+#     next_month_sales = model.predict([[pd.to_numeric((next_month - monthly_sales['month'].min()) / pd.offsets.MonthBegin(1))]])
+#     return next_month_sales[0]
